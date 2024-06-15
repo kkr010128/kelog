471df0327efa718c1a0844a8fcf7f67f2a472b5d
kkr010128 <74182629+kkr010128@users.noreply.github.com>
2024-06-15 18:09:57 +0900

    image

--- src/routes/Feed/ProfileCard.tsx
+++ src/routes/Feed/ProfileCard.tsx
@@ -14,7 +14,7 @@ const ProfileCard: React.FC<Props> = () => {
       </div>
       <div className="content">
         <div className="top">
-          <Image src={CONFIG.profile.image} fill alt="" />
+          <Image src={CONFIG.profile.image} width = {45} height = {45} fill alt="" />
         </div>
         <div className="mid">
           <div className=" name">{CONFIG.profile.name}</div>
