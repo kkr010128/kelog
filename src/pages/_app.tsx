d2184d1b555f715d78468dd4e31a5fd23ec63ee6
kkr010128 <74182629+kkr010128@users.noreply.github.com>
2024-06-17 18:01:04 +0900

    SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,20 +3,13 @@ import { Hydrate, QueryClientProvider } from "@tanstack/react-query"
 import { RootLayout } from "src/layouts"
 import { queryClient } from "src/libs/react-query"
 
-import Head from 'next/head'
-
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page)
 
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <RootLayout>
-          <Head>
-            <meta name="google-site-verification" content="rnyVBPK-KqC4bRK_La_EtOk25IocDxB7ZC1zuOBM69E" />
-            </Head>
-          {getLayout(<Component {...pageProps} />)}
-        </RootLayout>
+        <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
       </Hydrate>
     </QueryClientProvider>
   )
